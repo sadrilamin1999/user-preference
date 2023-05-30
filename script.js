390a89b2b37ec3a43d3a6e19c6fe266f22e136c4
SadrilAmin1999 <sadrilshuvo420@gmail.com>
2023-05-31 03:01:26 +0600

    reset handler and local storage added

--- script.js
+++ script.js
@@ -1,16 +1,25 @@
 const selectFontSize = document.getElementById("selectFontSize");
 const selectBgColor = document.getElementById("selectBgColor");
-const resetBtn = document.getElementById("resetBtn");
+// const resetBtn = document.getElementById("resetBtn");
+let resetBtn = document.getElementById("resetBtn");
 let main = document.getElementById("main");
 
 // font size change handler
 selectFontSize.addEventListener("change", function (event) {
   const font = event.target.value;
   main.style.fontSize = font;
+  localStorage.setItem("fontSize", font);
 });
 
 // background color change handler
 selectBgColor.addEventListener("change", function (event) {
   const color = event.target.value;
   main.style.backgroundColor = color;
+  localStorage.setItem("bgColor", color);
+});
+
+// reset handler
+resetBtn.addEventListener("click", function () {
+  localStorage.removeItem("fontSize");
+  localStorage.removeItem("bgColor");
 });
