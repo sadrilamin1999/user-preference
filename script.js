b411f7e1ca7c06b56e55023e7f7603961d46961d
SadrilAmin1999 <sadrilshuvo420@gmail.com>
2023-05-31 16:17:15 +0600

    user preference added

--- script.js
+++ script.js
@@ -1,8 +1,32 @@
 const selectFontSize = document.getElementById("selectFontSize");
 const selectBgColor = document.getElementById("selectBgColor");
-// const resetBtn = document.getElementById("resetBtn");
-let resetBtn = document.getElementById("resetBtn");
+const resetBtn = document.getElementById("resetBtn");
 let main = document.getElementById("main");
+
+const setValues = (fontSize, bgColor) => {
+  selectFontSize.value = fontSize;
+  selectBgColor.value = bgColor;
+  main.style.fontSize = fontSize;
+  main.style.backgroundColor = bgColor;
+};
+// load local storege
+const initialSetup = () => {
+  const bgColor = localStorage.getItem("bgColor");
+  const fontSize = localStorage.getItem("fontSize");
+
+  if (bgColor && fontSize) {
+    setValues(fontSize, bgColor);
+  }
+  if (!bgColor && !fontSize) {
+    setValues("16px", "aqua");
+  }
+  if (!bgColor && fontSize) {
+    setValues(fontSize, "aqua");
+  }
+  if (bgColor && !fontSize) {
+    setValues("16px", bgColor);
+  }
+};
 
 // font size change handler
 selectFontSize.addEventListener("change", function (event) {
@@ -22,4 +46,7 @@ selectBgColor.addEventListener("change", function (event) {
 resetBtn.addEventListener("click", function () {
   localStorage.removeItem("fontSize");
   localStorage.removeItem("bgColor");
+  setValues("16px", "aqua");
 });
+
+initialSetup();
